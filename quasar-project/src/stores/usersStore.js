d046355ded0589e7549441ec1a859cafb781e293
Łukasz Kuleński <lukasz.kulenski17@op.pl>
2024-02-19 16:33:01 +0100

    commit

--- quasar-project/src/stores/usersStore.js
+++ quasar-project/src/stores/usersStore.js
@@ -144,7 +144,6 @@ export const useUsersStore = defineStore('usersStore', () => {
                 $q.loading.show()
 
                 const id = user.uid;
-                console.log('user:', user)
 
                 get(dbRef(db, `users/${id}/firebaseUserDetails`)).then(snapshot => {
                     if (snapshot.exists()) {
@@ -218,28 +217,7 @@ export const useUsersStore = defineStore('usersStore', () => {
         const provider = new GoogleAuthProvider();
         provider.addScope('https://www.googleapis.com/auth/userinfo.email');
 
-        // signInWithRedirect(auth, provider)
-        signInWithPopup(auth, provider)
-        .then((result) => {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          const credential = GoogleAuthProvider.credentialFromResult(result);
-          const token = credential.accessToken;
-          // The signed-in user info.
-          const user = result.user;
-          // IdP data available using getAdditionalUserInfo(result)
-          // ...
-
-          console.log(user)
-        }).catch((error) => {
-          // Handle Errors here.
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          // The email of the user's account used.
-          const email = error.customData.email;
-          // The AuthCredential type that was used.
-          const credential = GoogleAuthProvider.credentialFromError(error);
-          // ...
-        });
+        signInWithRedirect(auth, provider)
     }
 
     return {
